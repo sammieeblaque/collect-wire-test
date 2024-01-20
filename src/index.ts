446bffe09d6c783dfde1fca2885ba0a3568ce084
Samuel Airehrour <sairehrour@gmail.com>
2024-01-20 17:29:28 +0100

    chore: updated the PORT

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8088;
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port} 🔥`);
