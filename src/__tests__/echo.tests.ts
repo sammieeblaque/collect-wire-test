736d21e9cbe2e03ef749d3ee15ba98435d46a455
Samuel Airehrour <sairehrour@gmail.com>
2024-01-20 13:55:29 +0100

    chore: updated tests

--- src/__tests__/echo.tests.ts
+++ src/__tests__/echo.tests.ts
@@ -7,17 +7,51 @@ import app from "../app";
 
 const filePath = path.resolve("matrix.csv");
 
-describe("a functions adds correctly", () => {
-  test("add two numbers", () => {
-    expect(add(2, 3)).toBe(5);
-  });
-});
-
-describe("testing endpoints", () => {
+describe("Testing endpoints returns correct values", () => {
   test("testing echo endpoint", async () => {
     const response = await request(app).post("/echo").attach("file", filePath);
     expect(response.statusCode).toBe(200);
     expect(response.text).toMatch("1,2,3\n4,5,6\n7,8,9");
+    expect(response.ok).toBe(true);
+    expect(response.body).toBeTruthy();
+    expect(response.type).toMatch("text/html");
+  });
+
+  test("testing inverse endpoint", async () => {
+    const response = await request(app)
+      .post("/inverse")
+      .attach("file", filePath);
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toMatch("1,4,7\n2,5,8\n3,6,9");
+    expect(response.ok).toBe(true);
+    expect(response.body).toBeTruthy();
+    expect(response.type).toMatch("text/html");
+  });
+
+  test("testing multiply endpoint", async () => {
+    const response = await request(app)
+      .post("/multiply")
+      .attach("file", filePath);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(362880);
+    expect(response.body).toBeTruthy();
+    expect(response.type).toMatch("application/json");
+  });
+
+  test("testing sum endpoint", async () => {
+    const response = await request(app).post("/sum").attach("file", filePath);
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual(45);
+    expect(response.body).toBeTruthy();
+    expect(response.type).toMatch("application/json");
+  });
+
+  test("testing flatten  endpoint", async () => {
+    const response = await request(app)
+      .post("/flatten")
+      .attach("file", filePath);
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toMatch("1,2,3,4,5,6,7,8,9");
     expect(response.ok).toBe(true);
     expect(response.body).toBeTruthy();
     expect(response.type).toMatch("text/html");
