811f946501d4f565d1257f8ce6fd2bec34c86526
Samuel Airehrour <sairehrour@gmail.com>
2024-01-20 14:00:27 +0100

    chore: added file exist check

--- src/__tests__/echo.tests.ts
+++ src/__tests__/echo.tests.ts
@@ -1,59 +1,80 @@
 import request from "supertest";
 import fs from "fs";
 import path from "path";
-import { add } from "../utils";
 
 import app from "../app";
 
-const filePath = path.resolve("matrix.csv");
-
 describe("Testing endpoints returns correct values", () => {
+  const filePath = path.resolve("matrix.csv");
+
   test("testing echo endpoint", async () => {
-    const response = await request(app).post("/echo").attach("file", filePath);
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toMatch("1,2,3\n4,5,6\n7,8,9");
-    expect(response.ok).toBe(true);
-    expect(response.body).toBeTruthy();
-    expect(response.type).toMatch("text/html");
+    if (!fs.existsSync(filePath)) {
+      throw new Error("file does not exist");
+    } else {
+      const response = await request(app)
+        .post("/echo")
+        .attach("file", filePath);
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toMatch("1,2,3\n4,5,6\n7,8,9");
+      expect(response.ok).toBe(true);
+      expect(response.body).toBeTruthy();
+      expect(response.type).toMatch("text/html");
+    }
   });
 
   test("testing inverse endpoint", async () => {
-    const response = await request(app)
-      .post("/inverse")
-      .attach("file", filePath);
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toMatch("1,4,7\n2,5,8\n3,6,9");
-    expect(response.ok).toBe(true);
-    expect(response.body).toBeTruthy();
-    expect(response.type).toMatch("text/html");
+    if (!fs.existsSync(filePath)) {
+      throw new Error("file does not exist");
+    } else {
+      const response = await request(app)
+        .post("/inverse")
+        .attach("file", filePath);
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toMatch("1,4,7\n2,5,8\n3,6,9");
+      expect(response.ok).toBe(true);
+      expect(response.body).toBeTruthy();
+      expect(response.type).toMatch("text/html");
+    }
   });
 
   test("testing multiply endpoint", async () => {
-    const response = await request(app)
-      .post("/multiply")
-      .attach("file", filePath);
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(362880);
-    expect(response.body).toBeTruthy();
-    expect(response.type).toMatch("application/json");
+    if (!fs.existsSync(filePath)) {
+      throw new Error("file does not exist");
+    } else {
+      const response = await request(app)
+        .post("/multiply")
+        .attach("file", filePath);
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual(362880);
+      expect(response.body).toBeTruthy();
+      expect(response.type).toMatch("application/json");
+    }
   });
 
   test("testing sum endpoint", async () => {
-    const response = await request(app).post("/sum").attach("file", filePath);
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual(45);
-    expect(response.body).toBeTruthy();
-    expect(response.type).toMatch("application/json");
+    if (!fs.existsSync(filePath)) {
+      throw new Error("file does not exist");
+    } else {
+      const response = await request(app).post("/sum").attach("file", filePath);
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual(45);
+      expect(response.body).toBeTruthy();
+      expect(response.type).toMatch("application/json");
+    }
   });
 
   test("testing flatten  endpoint", async () => {
-    const response = await request(app)
-      .post("/flatten")
-      .attach("file", filePath);
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toMatch("1,2,3,4,5,6,7,8,9");
-    expect(response.ok).toBe(true);
-    expect(response.body).toBeTruthy();
-    expect(response.type).toMatch("text/html");
+    if (!fs.existsSync(filePath)) {
+      throw new Error("file does not exist");
+    } else {
+      const response = await request(app)
+        .post("/flatten")
+        .attach("file", filePath);
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toMatch("1,2,3,4,5,6,7,8,9");
+      expect(response.ok).toBe(true);
+      expect(response.body).toBeTruthy();
+      expect(response.type).toMatch("text/html");
+    }
   });
 });
